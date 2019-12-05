b9fcd69816aeef6b08645299992a7a0011551b08
AnnuSingh1 <annusingh12333@gmail.com>
2019-12-05 19:13:26 -0300

    My profile done Customer support done My bookng done

--- app/src/main/java/com/sundram/urbanclapclone/home_fragment.java
+++ app/src/main/java/com/sundram/urbanclapclone/home_fragment.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 
 public class home_fragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
-
     public home_fragment() {
         // Required empty public constructor
     }
@@ -28,33 +26,5 @@ public class home_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.activity_home_screen, container, false);
-    }
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }

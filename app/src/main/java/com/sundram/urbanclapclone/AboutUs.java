a8fedcb264678e1513ffbe7584265eb312998c4c
AnnuSingh1 <annusingh12333@gmail.com>
2019-12-05 14:46:49 -0300

    Home Screen Completed

--- app/src/main/java/com/sundram/urbanclapclone/AboutUs.java
+++ app/src/main/java/com/sundram/urbanclapclone/AboutUs.java
@@ -1,14 +1,22 @@
 package com.sundram.urbanclapclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class AboutUs extends AppCompatActivity {
 
+    Toolbar aboutUsToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        aboutUsToolbar = findViewById(R.id.custom_toolbar_wedget);
+        setSupportActionBar(aboutUsToolbar);
+        if(getSupportActionBar()!=null) {
+          getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        aboutUsToolbar.setTitle("AboutUs");
     }
 }

f1c2f8ce3e1f9b587aca63dbbbb6f394310f94d6
AnnuSingh1 <annusingh12333@gmail.com>
2019-12-05 16:13:14 -0300

    My profile done Customer support done My bookng done

--- app/src/main/java/com/sundram/urbanclapclone/OtpGeneratorActivity.java
+++ app/src/main/java/com/sundram/urbanclapclone/OtpGeneratorActivity.java
@@ -6,9 +6,10 @@ import android.os.Bundle;
 
 public class OtpGeneratorActivity extends AppCompatActivity {
 
+    private final int time_out=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_otp_verification);
+        setContentView(R.layout.otp_generator_activity);
     }
 }
